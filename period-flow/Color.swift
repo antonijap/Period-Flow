997bdba7d220d702abc1ed19146f20441373b2ff
Antonija <ipekica@gmail.com>
2016-05-31 18:37:54 +0200

    Basic UI completed

---
+++ period-flow/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  period-flow
+//
+//  Created by Antonija Pek on 31/05/16.
+//  Copyright © 2016 Antonija Pek. All rights reserved.
+//
+
+import UIKit
+
+enum Color {
+    static let red =  UIColor(red:1.000, green: 0.420, blue: 0.420, alpha: 1.000)
+    static let blue =  UIColor(red:0.490, green: 0.588, blue: 0.725, alpha: 1.000)
+    static let lightGrey =  UIColor(red:0.945, green: 0.969, blue: 1.000, alpha: 1.000)
+}
