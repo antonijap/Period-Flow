a02925997c34cec9a18490f2dc832e08a30310bf
Steven Deutsch <stevensdeutsch@yahoo.com>
2016-06-27 14:54:06 -0500

    Refactor PeriodAnalysisManager

--- period-flow/PeriodAnalysisManager.swift
+++ period-flow/PeriodAnalysisManager.swift
@@ -13,50 +13,47 @@ class PeriodAnalysisManager {
     
     // MARK: - Methods
     
-    class func performAnalysis(operation: (Slice<Results<Period>>, Double) -> Double) -> Double? {
-        guard let periods = RealmManager.sharedInstance.queryAllPeriods() where periods.count > 0 else {
+    class func getAveragePeriodDuration() -> Double? {
+        guard let periods = RealmManager.sharedInstance.queryPeriodsForAnalysis() else {
             return nil
         }
-        let durationBasis = DefaultsManager.getAnalysisNumber()
-        let endIndex = periods.endIndex.predecessor()
-        let startIndex = calculateStartIndex(periods, durationBasis: durationBasis)
-        
-        return operation(periods[startIndex...endIndex], Double(durationBasis))
-        
-    }
-    
-    class func calculateStartIndex(periods: Results<Period>, durationBasis: Int) -> Int {
-        switch periods.count {
-            case 1: return 0
-            case 2: return durationBasis - 1
-            default: return periods.count - durationBasis
-        }
-    }
-    
-    class func averagePeriodDuration(periods: Slice<Results<Period>>, durationBasis: Double) -> Double {
         let totalDays = periods.reduce(0) { (total, period) in
-            return total + period.assumedDates.count
+            total + period.assumedDates.count
         }
-        print(totalDays)
-        return Double(totalDays) / durationBasis
+        return Double(totalDays) / Double(DefaultsManager.getAnalysisNumber())
     }
     
-    
-    class func getAverageCycleDuration(periods: Slice<Results<Period>>, durationBasis: Double) -> Double {
+    class func getAverageCycleDuration() -> Double? {
+        guard let periods = RealmManager.sharedInstance.queryPeriodsForAnalysis() else {
+            return nil
+        }
         let totalDays = periods.reduce(0) { (total, period) in
             let daysBetween = daysBetweenDate(period.startDate!, endDate: period.endDate!)
-            return abs(daysBetween)
+            return total + abs(daysBetween)
         }
-        return Double(totalDays) / durationBasis
+        return Double(totalDays) / Double(DefaultsManager.getAnalysisNumber())
     }
     
+//    class func performAnalysis(operation: (Slice<Results<Period>>, Double) -> Double) -> Double? {
+//        guard let periods = RealmManager.sharedInstance.queryAllPeriods() where periods.count > 0 else {
+//            return nil
+//        }
+//        let durationBasis = DefaultsManager.getAnalysisNumber()
+//        let endIndex = periods.endIndex.predecessor()
+//        let startIndex = calculateStartIndex(periods, durationBasis: durationBasis)
+//        
+//        return operation(periods[startIndex...endIndex], Double(durationBasis))
+//        
+//    }
+    
+    // MARK: - Helper Methods
+    
     /// Gets number of days between two NSDates as Int value
-    class func daysBetweenDate(startDate: NSDate, endDate: NSDate) -> Int {
+    private class func daysBetweenDate(startDate: NSDate, endDate: NSDate) -> Int {
         let calendar = NSCalendar.currentCalendar()
         let start = calendar.startOfDayForDate(startDate)
         let end = calendar.startOfDayForDate(endDate)
         let components = calendar.components([.Day], fromDate: start, toDate: end, options: [])
         return components.day
     }
-
 }
