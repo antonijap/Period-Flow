a5df7495d86a2eae1fc0f2ce31f8ccdb3413f4b0
Steven Deutsch <stevensdeutsch@yahoo.com>
2016-06-28 13:24:10 -0500

    Refactor settings screen

--- period-flow/SettingsViewController.swift
+++ period-flow/SettingsViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import ActionSheetPicker_3_0
 
 class SettingsViewController: UIViewController {
 
@@ -24,31 +25,144 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var notificationButton: CustomButton!
     @IBOutlet weak var baseAnalysisButton: CustomButton!
     
+    // MARK: - Properties
+    
+    var purchaseManager: PurchaseManager?
+    
     // MARK: - View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupPurchaseManager()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        configureAnalysisView()
+        configureLabels()
     }
     
     // MARK: Methods
     
-    // MARK: Actions
+    func setupPurchaseManager() {
+        purchaseManager = PurchaseManager()
+        purchaseManager?.requestProducts()
+    }
     
-    @IBAction func cycleDurationButtonTapped(sender: AnyObject) {
+    /// Triggers the in app purchase for PRO pack product
+    func purchaseProPackPressed() {
+        if let product = purchaseManager?.products.first where product.productIdentifier == PURCHASE_PROPACK {
+            purchaseManager?.createPayment(product)
+        }
+    }
+    
+    /// Factory method to create an ActionSheetStringPicker with nil cancel block and trailing completion
+    func actionSheetFactory(title: String, rows: [Int], indexSelected: Int, sender: AnyObject, completion: ActionStringDoneBlock) -> ActionSheetStringPicker {
+        return ActionSheetStringPicker(title: title, rows: rows, initialSelection: indexSelected, doneBlock: completion, cancelBlock: nil, origin: sender)
+    }
+    
+    /// Displays the picker to set the Cycle Duration
+    func displayDurationPicker() {
+        let title = "Cycle Duration"
+        let days = (1...100).map { $0 }
+        let index = DefaultsManager.getCycleDays()
+        
+        let picker = actionSheetFactory(title, rows: days, indexSelected: index, sender: cycleDurationButton) { (picker, int, object) in
+            if let object = object as? Int {
+                DefaultsManager.setCycleDays(object)
+                let text = object == 1 ? "1 day" : "\(object) days"
+                self.cycleDurationButton.setTitle(text, forState: .Normal)
+            }
+        }
+        picker.showActionSheetPicker()
+    }
+    
+    /// Displays the picker to set the number of days before a notification occurs
+    func displayNotificationPicker() {
+        let title = "Days Before"
+        let currentDuration = DefaultsManager.getCycleDays()
+        let days = (1...currentDuration).map { $0 }
+        let index = DefaultsManager.getNotificationDays() - 1
+        
+        let picker = actionSheetFactory(title, rows: days, indexSelected: index, sender: notificationButton) { (picker, int, object) in
+            if let object = object as? Int {
+                DefaultsManager.setNotificationDays(object)
+                LocalNotificationsManager.cancelAllNotifications()
+                LocalNotificationsManager.registerNotification()
+                let text = object == 1 ? "1 day before period starts" : "\(object) days before period starts"
+                self.notificationButton.setTitle(text, forState: .Normal)
+            }
+        }
+        picker.showActionSheetPicker()
+    }
+    
+    /// Displays the picker to set the number of periods to base the analysis on
+    func displayAnalysisPicker() {
+        let title = "Number of Periods"
+        let totalPeriods = RealmManager.sharedInstance.queryAllPeriods()?.count
+        let rangeStart = totalPeriods == 0 ? 0 : 1
+        let rangeCap = totalPeriods ?? 1
+        
+        let range = (rangeStart...rangeCap).map { $0 }
+        
+        
+        let picker = actionSheetFactory(title, rows: range, indexSelected: 0, sender: baseAnalysisButton) { (picker, int, object) in
+            if let object = object as? Int {
+                DefaultsManager.setAnalysisNumber(object)
+                self.configureAnalysisView()
+                let text = object == 1 ? "Last Period" : "Last \(object) periods"
+                self.baseAnalysisButton.setTitle(text, forState: .Normal)
+            }
+        }
+        picker.showActionSheetPicker()
+    }
+    
+    /// Configures the analyis view with the data
+    
+    func configureAnalysisView() {
+        if let avgPeriodDuration = PeriodAnalysisManager.getAveragePeriodDuration() {
+            averagePeriodDurationLabel.text = "\(avgPeriodDuration.toPlaces(1))"
+        }
+        if let avgCycleDuration = PeriodAnalysisManager.getAverageCycleDuration() {
+            averageCycleDurationLabel.text = "\(avgCycleDuration.toPlaces(1))"
+        }
+    }
+    
+    func configureLabels() {
+        let durationDays = DefaultsManager.getCycleDays()
+        let durationText = durationDays == 1 ? "1 day" : "\(durationDays) days"
+        self.cycleDurationButton.setTitle(durationText, forState: .Normal)
+        
+        let analysisBasis = DefaultsManager.getAnalysisNumber()
+        let analysisText = analysisBasis == 1 ? "Last Period" : "Last \(analysisBasis) periods"
+        self.baseAnalysisButton.setTitle(analysisText, forState: .Normal)
+        
+        let notifDays = DefaultsManager.getNotificationDays()
+        let notifText = notifDays == 1 ? "1 day before period starts" : "\(notifDays) days before period starts"
+        self.notificationButton.setTitle(notifText, forState: .Normal)
     }
 
+    // MARK: Actions
+
+
+    @IBAction func cycleDurationButtonTapped(sender: AnyObject) {
+        displayDurationPicker()
+    }
+    
     @IBAction func notificationButtonTapped(sender: AnyObject) {
+        displayNotificationPicker()
     }
     
     @IBAction func baseAnalysisButtonTapped(sender: AnyObject) {
+        displayAnalysisPicker()
     }
     
     @IBAction func purchaseTapped(sender: AnyObject) {
-        print("I want to purchase")
+        purchaseProPackPressed()
     }
     
     @IBAction func restoreButtonTapped(sender: AnyObject) {
-        
+        purchaseManager?.restoreTransactions()
     }
     
     @IBAction func backButtonTapped(sender: AnyObject) {
